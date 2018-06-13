5538cd4fd15a528f7218284fae16ed10a566314d
Anton Isakov <anton_isakov@epam.com>
2018-06-14 01:52:56 +0400

    DB is edited

---
+++ js/user.js
@@ -0,0 +1,60 @@
+module.exports = {
+  // "info": [
+  //   {
+  //     "id": "age",
+  //     "name": "Возраст",
+  //     "value": 0
+  //   },
+  //   {
+  //     "id": "sex",
+  //     "name": "Пол",
+  //     "value": ""
+  //   }
+  // ],
+  "places": {
+    "restrict": [],
+    "fav": []
+  },
+  "preferences": [
+    {
+      "id": "history",
+      "name": "История",
+      "value": 0
+    },
+    {
+      "id": "architecture",
+      "name": "Архитектура",
+      "value": 0
+    },
+    {
+      "id": "nature",
+      "name": "Природа",
+      "value": 0
+    },
+    {
+      "id": "food",
+      "name": "Еда",
+      "value": 0
+    },
+    {
+      "id": "modern",
+      "name": "Модерн",
+      "value": 0
+    },
+    {
+      "id": "culture",
+      "name": "Культура",
+      "value": 0
+    },
+    {
+      "id": "water",
+      "name": "Пляжный отдых",
+      "value": 0
+    },
+    {
+      "id": "active",
+      "name": "Активный отдых",
+      "value": 0
+    }
+  ]
+}
